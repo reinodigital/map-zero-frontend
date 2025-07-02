d9e09a65aa47323cc619258e69bfc6ef00b2f7a7
onier9702 <onier0217@gmail.com>
2025-07-01 19:18:03 -0600

    [update] purchase overview page finished

--- src/app/modules/business/overviews/purchases-overview/purchases-overview.component.ts
+++ src/app/modules/business/overviews/purchases-overview/purchases-overview.component.ts
@@ -1,11 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  signal,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { OverviewService } from '../../../../api/overview.service';
+
+import { IPurchasesOverviewStats } from '../../../../interfaces';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
   selector: 'purchases-overview',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './purchases-overview.component.html',
   styleUrl: './purchases-overview.component.scss',
 })
-export default class PurchasesOverviewComponent {}
+export default class PurchasesOverviewComponent {
+  private destroyRef = inject(DestroyRef);
+  private overviewService = inject(OverviewService);
+
+  public purchases = signal<IPurchasesOverviewStats | null>(null);
+  public objectKeys = Object.keys;
+
+  ngOnInit(): void {
+    this.overviewService
+      .fetchPurchasesOverview()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((resp) => {
+        if (resp) {
+          this.purchases.set(resp);
+        }
+      });
+  }
+}
